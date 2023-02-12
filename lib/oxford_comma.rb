f12a970825e02904a40a64c5597c663e04ee24a7
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-12 13:14:45 +0300

    Assignment completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      return "#{array[0]} and #{array[1]}"
+    else
+      last_element = array.pop
+      array.join(", ") + ", and " + last_element
+    end
+  end
+  
